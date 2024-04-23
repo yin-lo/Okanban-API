bfbde8c65710f50b5a85cd153270a88c673f6393
Laurent <l.neveux@icloud.com>
2024-04-23 11:40:18 +0200

    add REST list controller + int middleware

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -38,9 +38,86 @@ const listController = {
         res.json(list);
     },
 
-    store(req, res) {},
-    update(req, res) {},
-    destroy(req, res) {},
+    async store(req, res) {
+        // ! On doit valider les données qui viennent du client : on ne fait jamais confiance à ce qui vient du client, on n'utilisa pas req.body directement
+        // ! Idéalement, on validerai req.body dans un middleware
+        const { title, position } = req.body;
+
+        if (!title || typeof title !== 'string') {
+            return res
+                .status(400)
+                .json({ error: 'Le paramètre title est invalide' });
+        }
+
+        if (isDefinedButNotInt(position)) {
+            return res
+                .status(400)
+                .json({ error: 'Le paramètre position est invalide' });
+        }
+
+        const newList = await List.create({ title, position });
+
+        res.json({ list: newList });
+    },
+
+    async update(req, res) {
+        const { id } = req.params;
+        const { title, position } = req.body;
+
+        if (typeof title !== 'string') {
+            return res
+                .status(400)
+                .json({ error: 'Le paramètre title est invalide' });
+        }
+
+        // ! On gèrera la validation de position avec un module
+
+        // * Avant de mettre àjour, on doit récupérer une ressource : on s'assure que la liste existe
+        const listToUpdate = await List.findByPk(id);
+
+        // * Si la liste n'existe pas, on envoie un 404
+        if (!listToUpdate) {
+            return res.status(404).json({ error: "La liste n'existe pas" });
+        }
+
+        // * Sinon on la met à jour
+        const updatedList = await listToUpdate.update({
+            title: title || listToUpdate.title,
+            position: position || listToUpdate.position,
+        });
+
+        return res.json({ list: updatedList });
+    },
+
+    async destroy(req, res) {
+        const id = Number.parseInt(req.params.id, 10);
+
+        if (!Number.isInteger(id)) {
+            return res.status(204).json({ error: "La ressource n'existe pas" });
+        }
+
+        const list = await List.findByPk(id);
+
+        if (!list) {
+            return res.status(204).json({ error: "La ressource n'existe pas" });
+        }
+
+        await list.destroy();
+
+        return res.json({ message: 'La ressource a été effacé' });
+    },
 };
+
+/**
+ *
+ * cette fonction va vérifier si value est undefined ou non, et si value est un integer positif
+ *
+ * @param {int} value
+ * @returns Boolean
+ */
+function isDefinedButNotInt(value) {
+    // value doit être défini, un integer et supérieur à 0
+    return value !== undefined && (!Number.isInteger(value) || value <= 0);
+}
 
 export { listController };
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 import { mainController } from '../controllers/mainController.js';
 import { listController } from '../controllers/listController.js';
-
+import { isNumberMiddleware } from '../middlewares/validators.js';
 // DONE
 router.get('/', mainController.index);
 // * - `index` : sert à obtenir une liste de ressources : GET : Model.findAll
@@ -10,15 +10,15 @@ router.get('/', mainController.index);
 router.get('/lists', listController.index);
 // * - `show` : on obtient le détail d'une ressource : GET Model.findByPk
 // DONE
-router.get('/lists/:id', listController.show);
+router.get('/lists/:id', isNumberMiddleware, listController.show);
 // * - `store` : on persiste la ressource en BDD : POST ou PUT Model.create
 // TODO
 router.post('/lists', listController.store);
 // * - `update` : on persiste la mise à jour : PUT ou PATCH Model.update
 // TODO
-router.patch('/lists/:id', listController.update);
+router.patch('/lists/:id', isNumberMiddleware, listController.update);
 // TODO
 // * - `destroy` : on efface une donnée : DELETE Model.destroy
-router.delete('/lists/:id', listController.destroy);
+router.delete('/lists/:id', isNumberMiddleware, listController.destroy);
 
 export { router };
